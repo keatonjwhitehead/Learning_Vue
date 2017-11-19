0f70265796970d5b3d6e888feeb5fd4cfbfaf38c
keatonjwhitehead <keaton.j.whitehead@gmail.com>
2017-11-19 00:17:11 -0700

    Dynamic CSS Classes

--- app.js
+++ app.js
@@ -121,3 +121,21 @@ new Vue({
           }
      }
 })
+new Vue({
+     el:"#vue-app-dynamic-css-classes",
+     data:{
+          available:false,
+          nearby:false
+     },
+     methods:{
+
+     },
+     computed:{
+          compClasses:function(){
+               return{
+                    available:this.available,
+                    nearby:this.nearby
+               }
+          }
+     }
+})
