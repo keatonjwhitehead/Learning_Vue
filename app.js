1cec65aaad78d23d6615964a0b029f7f965d6054
keatonjwhitehead <keaton.j.whitehead@gmail.com>
2017-11-18 23:47:41 -0700

    Computed Properties

--- app.js
+++ app.js
@@ -100,3 +100,24 @@ new Vue({
           }
      }
 })
+new Vue({
+     el: "#vue-app-Computed-Properties",
+     data: {
+          age:20,
+          a: 0,
+          b: 0
+     },
+     methods: {
+
+     },
+     computed:{
+          addToA:function(){
+               console.log('addToA')
+               return this.a + this.age;
+          },
+          addToB:function(){
+               console.log('addToB')
+               return this.b + this.age
+          }
+     }
+})
