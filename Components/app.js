b1f06242533839494499d848c02a18d0f82feaa5
Keat <Keaton.j.whitehead@gmail.com>
2018-01-12 09:39:45 -0700

    Intro to Components

--- Components/app.js
+++ Components/app.js
@@ -1,9 +1,19 @@
 Vue.component('greeting', {
-     template:'<p>Hey there, I am a re-usable component</p>'
+     template:'<p>Hey there, I am {{ name }}. <button v-on:click="changeName">Change name</button></p>',
+     data:function() {
+          return{
+               name:'Keaton'
+          }
+     },
+     methods:{
+          changeName:function(){
+               this.name = 'Mario';
+          }
+     }
 });
 
 new Vue ({
-     el: '#vue-app-one'
+     el: '#vue-app-one',
 });
 
 new Vue ({
